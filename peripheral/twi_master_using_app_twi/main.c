15c4c3324e7e472e64ceb177b82b651d651f704a
e27182 <gennadii.kurabko@gmail.com>
2016-09-13 23:53:34 +0300

    TWI example

---
+++ peripheral/twi_master_using_app_twi/main.c
@@ -0,0 +1,152 @@
+#include "nrf_drv_rtc.h"
+#include "nrf_drv_clock.h"
+#include "app_twi.h"
+#define NRF_LOG_MODULE_NAME "APP"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_delay.h"
+
+#define SDA_PIN                     6
+#define SCL_PIN                     7
+#define MPU_PWR_MGMT_1_REG          0x6B
+#define MPU_WHO_AM_I_REG            0x75
+#define MPU_I2C_ADDRESS             0x68
+#define BIT_RESET                   0x80
+
+#define MAX_PENDING_TRANSACTIONS    5
+
+static app_twi_t m_app_twi = APP_TWI_INSTANCE(0);
+
+static nrf_drv_rtc_t const m_rtc = NRF_DRV_RTC_INSTANCE(0);
+
+uint8_t const mpu_pwr_mgmt_1_reg_addr = MPU_PWR_MGMT_1_REG;
+uint8_t const mpu_who_am_i_reg_addr = MPU_WHO_AM_I_REG;
+
+#define BUFFER_SIZE  3
+static uint8_t m_buffer[BUFFER_SIZE] = { BIT_RESET, 0x00, 0x00 };
+
+static app_twi_transfer_t const mpu_init_transfers[] = 
+{
+    APP_TWI_WRITE(MPU_I2C_ADDRESS, &mpu_pwr_mgmt_1_reg_addr, 1, APP_TWI_NO_STOP),
+    APP_TWI_WRITE(MPU_I2C_ADDRESS, &m_buffer[0], 1, 0),
+
+    APP_TWI_WRITE(MPU_I2C_ADDRESS, &mpu_pwr_mgmt_1_reg_addr, 1, APP_TWI_NO_STOP),
+    APP_TWI_WRITE(MPU_I2C_ADDRESS, &m_buffer[1], 1, 0),
+};
+
+void read_cb(ret_code_t result, void * p_user_data)
+{
+    if (result != NRF_SUCCESS)
+    {
+        NRF_LOG_INFO("read_cb - error: %d\r\n", (int)result);
+        return;
+    }
+
+    uint8_t whoami = m_buffer[2];
+    NRF_LOG_INFO("WHOAMI: %d\r\n", (int)whoami);
+}
+static void read(void)
+{
+    // [these structures have to be "static" - they cannot be placed on stack
+    //  since the transaction is scheduled and these structures most likely
+    //  will be referred after this function returns]
+    static app_twi_transfer_t const transfers[] =
+    {
+        APP_TWI_WRITE(MPU_I2C_ADDRESS, &mpu_who_am_i_reg_addr, 1, APP_TWI_NO_STOP),
+        APP_TWI_READ (MPU_I2C_ADDRESS, &m_buffer[2], 1, 0)
+    };
+
+    static app_twi_transaction_t const transaction =
+    {
+        .callback            = read_cb,
+        .p_user_data         = NULL,
+        .p_transfers         = transfers,
+        .number_of_transfers = 2
+    };
+    APP_ERROR_CHECK(app_twi_schedule(&m_app_twi, &transaction));
+}
+
+// TWI (with transaction manager) initialization.
+static void twi_config(void)
+{
+    uint32_t err_code;
+
+    nrf_drv_twi_config_t const config = {
+       .scl                = SCL_PIN,
+       .sda                = SDA_PIN,
+       .frequency          = NRF_TWI_FREQ_400K,
+       .interrupt_priority = APP_IRQ_PRIORITY_LOW,
+       .clear_bus_init     = false
+    };
+
+    APP_TWI_INIT(&m_app_twi, &config, MAX_PENDING_TRANSACTIONS, err_code);
+    APP_ERROR_CHECK(err_code);
+}
+
+
+// RTC tick events generation.
+static void rtc_handler(nrf_drv_rtc_int_type_t int_type)
+{
+    if (int_type == NRF_DRV_RTC_INT_TICK)
+    {
+        // On each RTC tick (their frequency is set in "nrf_drv_config.h")
+        // we read data from our sensor.
+        read();
+    }
+}
+static void rtc_config(void)
+{
+    uint32_t err_code;
+
+    // Initialize RTC instance with default configuration.
+    nrf_drv_rtc_config_t config = NRF_DRV_RTC_DEFAULT_CONFIG;
+    config.prescaler = RTC_FREQ_TO_PRESCALER(32); //Set RTC frequency to 32Hz
+    err_code = nrf_drv_rtc_init(&m_rtc, &config, rtc_handler);
+    APP_ERROR_CHECK(err_code);
+
+    // Enable tick event and interrupt.
+    nrf_drv_rtc_tick_enable(&m_rtc, true);
+
+    // Power on RTC instance.
+    nrf_drv_rtc_enable(&m_rtc);
+}
+
+
+static void lfclk_config(void)
+{
+    uint32_t err_code;
+
+    err_code = nrf_drv_clock_init();
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_clock_lfclk_request(NULL);
+}
+
+
+int main(void)
+{
+    // Start internal LFCLK XTAL oscillator - it is needed for "read" ticks generation (by RTC).
+    lfclk_config();
+
+    APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
+
+    NRF_LOG_INFO("TWI master example\r\n");
+    NRF_LOG_FLUSH();
+    twi_config();
+
+    // Initialize sensors.
+    APP_ERROR_CHECK(app_twi_perform(&m_app_twi, &mpu_init_transfers[0], 2, NULL));
+    nrf_delay_ms(100);
+    APP_ERROR_CHECK(app_twi_perform(&m_app_twi, &mpu_init_transfers[2], 2, NULL));
+
+    rtc_config();
+
+    while (true)
+    {
+        __WFI();
+        NRF_LOG_FLUSH();
+    }
+}
+
+
+/** @} */
