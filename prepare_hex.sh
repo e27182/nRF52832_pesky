3bf94e9b395a35b8e5f2cc0d81af408f61fed92e
e27182 <gennadii.kurabko@gmail.com>
2016-10-26 00:31:34 +0300

    Fix names of HEX files

--- prepare_hex.sh
+++ prepare_hex.sh
@@ -1,18 +1,14 @@
-proj_dir=$PWD
 hex_dir=../../../hex
 dirs=($(find . -name armgcc -type d))
 
 for dir in "${dirs[@]}"; do
     echo
-    pushd "$dir"
-    echo
-    # make clean
-    make
-    hex_name=$(echo $dir | sed "s|./||; s|$PWD/[^/]*/||; s|/armgcc|.hex|; s|/|_|g")
-    mkdir -pv $hex_dir
-    echo
-    cp -pvu _build/nrf52832_xxaa.hex $hex_dir/$hex_name
-    popd
-    echo
+    pushd "$dir" && echo
+    # make clean && echo
+    make && echo
+    hex_name=$(echo $dir | sed "s|./[^/]*/||; s|/armgcc|.hex|; s|/|_|g")
+    mkdir -pv $hex_dir  && echo
+    cp -pvu _build/nrf52832_xxaa.hex $hex_dir/$hex_name  && echo
+    popd && echo
     echo ------------------------
 done
